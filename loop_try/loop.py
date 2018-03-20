fa5372210419afd5d2db7762fb6acebade3bb79b
TigerJlx <2274701670@qq.com>
2018-03-20 15:28:08 +0800

    初步实现循环嵌套（但有递归深度的限制）

--- loop_try/loop.py
+++ loop_try/loop.py
@@ -1,9 +1,10 @@
-from loop_try.loop_base import FuncStructObj, IfBlockCode, OnelineCode, LinesCode, LoopCode
+from loop_try.loop_base22 import FuncStructObj, IfBlockCode, OnelineCode, LinesCode, LoopCode, FuncBlock
 from graphviz import Digraph
 import os
 
 os.environ["PATH"] += os.pathsep + r"C:\Program Files (x86)\Graphviz2.38\bin"
 y_res, n_res = [], []
+
 
 def ifblock_tail_process(y_res, n_res, node_name):
     for member in y_res:
@@ -19,7 +20,7 @@ def ifblock_tail_process(y_res, n_res, node_name):
             dot.edge(member[0], node_name, label="N")
 
 
-def code_block_process(obj):
+def code_block_process(obj, **options):
     global y_res, n_res
     items = obj.items
     # 处理每个item,并获取每个item首尾nodes
@@ -27,7 +28,7 @@ def code_block_process(obj):
         index = str(items.index(i))
         node_name = index + obj.name
         if type(i) == IfBlockCode:
-            Ifblock_code_process(i, node_name)
+            Ifblock_code_process(i, node_name, **options)
             i.y_res, i.n_res = y_res.copy(), n_res.copy()
             y_res, n_res = [], []
         elif type(i) in (OnelineCode, LinesCode):
@@ -40,7 +41,7 @@ def code_block_process(obj):
     # 将每个item首尾nodes串起来
     for i in items:
         index = items.index(i)
-        if index > 0:
+        if index > 0 and i.__str__() not in ("continue", "break"):
             last_item = items[index - 1]
             this_item_head = str(index) + obj.name
             if type(last_item) == IfBlockCode:
@@ -55,10 +56,11 @@ def code_block_process(obj):
 
 def linde_code_process(item, index):
     node_name = index
-    dot.node(node_name, item.__str__())
+    if item.__str__() not in ("continue", "break"):
+        dot.node(node_name, item.__str__())
 
 
-def Ifblock_code_process(item, index):
+def Ifblock_code_process(item, index, **options):
     global y_res, n_res
     node_name = index
     dot.node(node_name, item.condition, {'shape': "diamond"})
@@ -66,11 +68,37 @@ def Ifblock_code_process(item, index):
         if type(item.if_true) == IfBlockCode:
             Ifblock_code_process(item.if_true, node_name + 'if_sub')
             dot.edge(node_name, node_name + 'if_sub', label="Y")
-        elif type(item.if_true) in (OnelineCode, LinesCode):
-            node_name_true = node_name + 'if_true'
-            dot.node(node_name_true, item.if_true.__str__())
-            dot.edge(node_name, node_name_true, label="Y")
-            y_res.append((node_name_true, True))
+        elif type(item.if_true) == OnelineCode:
+            if item.if_true.__str__() == "break":
+                item.if_true = None
+                dot.edge(node_name, options["break_node_name"], label="Y")
+            elif item.if_true.__str__() == "continue":
+                item.if_true = None
+                dot.edge(node_name, options["continue_node_name"], label="Y")
+            else:
+                node_name_true = node_name + 'if_true'
+                dot.node(node_name_true, item.if_true.__str__())
+                dot.edge(node_name, node_name_true, label="Y")
+                y_res.append((node_name_true, True))
+        elif type(item.if_true) == LinesCode:
+            last_member = item.if_true.items[-1]
+            if last_member.__str__() == "break":
+                item.if_true.items.pop(-1)
+                node_name_true = node_name + 'if_true'
+                dot.node(node_name_true, item.if_true.__str__())
+                dot.edge(node_name, node_name_true)
+                dot.edge(node_name_true, options["break_node_name"], label="Y")
+            elif last_member.__str__() == "continue":
+                item.if_true.items.pop(-1)
+                node_name_true = node_name + 'if_true'
+                dot.node(node_name_true, item.if_true.__str__())
+                dot.edge(node_name, node_name_true)
+                dot.edge(node_name_true, options["continue_node_name"])
+            else:
+                node_name_true = node_name + 'if_true'
+                dot.node(node_name_true, item.if_true.__str__())
+                dot.edge(node_name, node_name_true, label="Y")
+                y_res.append((node_name_true, True))
         else:
             pass
     else:
@@ -85,6 +113,19 @@ def Ifblock_code_process(item, index):
             dot.node(node_name_false, item.if_false.__str__())
             dot.edge(node_name, node_name_false, label="N")
             n_res.append((node_name_false, True))
+        elif type(item.if_false) == FuncBlock:
+            node_name_false = str(0) + item.if_false.name
+            code_block_process(item.if_false)
+            dot.edge(node_name, node_name_false, label="N")
+            if type(item.if_false.items[-1]) in (LinesCode, OnelineCode):
+                n_res.append(((str(len(item.if_false.items) - 1) + item.if_false.name), True))
+            elif type(item.if_false.items[-1]) == IfBlockCode:
+                n_res.append(((item.if_false.items[-1].y_res[0][0]), True))
+            elif type(item.if_false.items[-1]) == LoopCode:
+                n_res.append((item.if_false.items[-1].break_node_name, True))
+            else:
+                pass
+
         else:
             pass
     else:
@@ -94,11 +135,17 @@ def Ifblock_code_process(item, index):
 def loop_code_process(item, index):
     global y_res, n_res
     item.node_name = index
-    # init_block----------------------------------
     init_block = item.init_block
     ifblock = item.ifblock
     other_block = item.other_block
-    print(type(item.other_block), item.other_block)
+    # continue
+    item.continue_node_name = item.node_name + 'continue'
+    dot.node(item.continue_node_name, 'continue')
+    # break
+    item.break_node_name = item.node_name + 'break'
+    dot.node(item.break_node_name, 'break')
+
+    # init_block-------------------------------------------
     if init_block is None:
         ifblock.node_name = item.node_name
         dot.node(item.node_name, item.ifblock.condition, {'shape': "diamond"})
@@ -107,21 +154,24 @@ def loop_code_process(item, index):
         dot.node(ifblock.node_name, item.ifblock.condition, {'shape': "diamond"})
         dot.node(item.node_name, init_block.__str__())
         dot.edge(item.node_name, ifblock.node_name)
-    # ifblock----------------------------------
-    Ifblock_code_process(ifblock, ifblock.node_name)
+
+    # ifblock------------------------------------------------
+    options = {"continue_node_name": item.continue_node_name, "break_node_name": item.break_node_name}
+    Ifblock_code_process(ifblock, ifblock.node_name, **options)
     ifblock.y_res, ifblock.n_res = y_res.copy(), n_res.copy()
     y_res, n_res = [], []
-    item.break_node_name = ifblock.y_res[0][0]
+    # item.break_node_name = ifblock.y_res[0][0]
 
+    # other_block---------------------------------------------
     if other_block is not None:
-        code_block_process(other_block)
+        code_block_process(other_block, **options)
         other_block.head_node_name = str(0) + other_block.name
-        if type(other_block.items[-1]) in (LinesCode, OnelineCode):
-            other_block.tail_node_name = str(len(other_block.items) - 1) + other_block.name
-        elif type(other_block.items[-1]) == IfBlockCode:
-            other_block.tail_node_name = other_block.items[-1].y_res, other_block.items[-1].n_res
-        elif type(other_block.items[-1]) == LoopCode:
-            other_block.tail_node_name = other_block.items[-1].break_node_name
+        if type(other_block.items[-2]) in (LinesCode, OnelineCode):
+            other_block.tail_node_name = str(len(other_block.items) - 2) + other_block.name
+        elif type(other_block.items[-2]) == IfBlockCode:
+            other_block.tail_node_name = other_block.items[-2].y_res, other_block.items[-2].n_res
+        elif type(other_block.items[-2]) == LoopCode:
+            other_block.tail_node_name = other_block.items[-2].break_node_name
         else:
             pass
         item.other_block_head = other_block.head_node_name
@@ -130,30 +180,29 @@ def loop_code_process(item, index):
         item.other_block_head = ifblock.node_name
         item.other_block_tail = ifblock.node_name
 
-    # continue_block----------------------------------------
-    item.continue_node_name = item.node_name + 'continue'
-    dot.node(item.continue_node_name, 'continue')
-    if other_block is not None:
-        # ifblock的N,指向other_block的头
-        member = ifblock.n_res[0]
-        if member[1]:
-            dot.edge(member[0], item.other_block_head)
+    # other_block 与 continue_block 的连接-------------------
+    if other_block.items[-1].__str__() == "continue":
+        if other_block is not None:
+            # ifblock的N,指向other_block的头
+            member = ifblock.n_res[0]
+            if member[1]:
+                dot.edge(member[0], item.other_block_head)
+            else:
+                dot.edge(member[0], item.other_block_head, label="N")
+            # other_block的尾,指向continue_block
+            if type(item.other_block_tail) == tuple:
+                ifblock_tail_process(item.other_block_tail[0], item.other_block_tail[1], item.continue_node_name)
+            else:
+                dot.edge(item.other_block_tail, item.continue_node_name)
         else:
-            dot.edge(member[0], item.other_block_head, label="N")
-        # other_block的尾,指向continue_block
-        if type(item.other_block_tail) == tuple:
-            ifblock_tail_process(item.other_block_tail[0], item.other_block_tail[1], item.continue_node_name)
-        else:
-            dot.edge(item.other_block_tail, item.continue_node_name)
-    else:
-        # ifblock的N,指向continue_block
-        dot.edge(ifblock.n_res[0][0], item.continue_node_name, label="N")
-    # continue_block执行LoopCode头
-    dot.edge(item.continue_node_name, item.node_name)
+            # ifblock的N,指向continue_block
+            dot.edge(ifblock.n_res[0][0], item.continue_node_name, label="N")
+        # continue_block执行LoopCode头
+        dot.edge(item.continue_node_name, item.node_name)
 
 
 # 初始化
-dot = Digraph(comment=FuncStructObj.name, engine='dot')
+dot = Digraph(comment=FuncStructObj.name, engine='dot', format='svg')
 dot.edge_attr = {'comment': "Wildcard edge",
                  'fontname': "sans-serif",
                  'fontsize': '10',
@@ -174,5 +223,5 @@ print(dot.source)
 # Save and render the source code, optionally view the result:
 if os.path.exists(FuncStructObj.name + '.gv'):
     os.remove(FuncStructObj.name + '.gv')
-    os.remove(FuncStructObj.name + '.gv' + '.pdf')
+    os.remove(FuncStructObj.name + '.gv' + '.svg')
 dot.view(FuncStructObj.name + '.gv')
--- loop_try/loop_base.py
+++ loop_try/loop_base.py
@@ -44,7 +44,7 @@ class LoopCode:
         self.node_name = None
         self.init_block = None
         self.ifblock = None
-        self.other_block = None
+        self.other_block = None  # FuncBlock类型数据
         self.other_block_head = None
         self.other_block_tail = None
         self.break_node_name = None
@@ -52,9 +52,11 @@ class LoopCode:
 
 
 FuncStructObj = FuncBlock("Insieme_SpromVerifyMain")
-
-item1 = OnelineCode(r'ENTER "Insieme_SpromProgMain\\n"')
-item2 = OnelineCode('CLR_REM')
+item1 = LinesCode()
+item1.add_line(OnelineCode(r'ENTER "Insieme_SpromVerifyMain\\n"'))
+item1.add_line(OnelineCode(r'setstring test "SPROM VERIFY"'))
+item1.add_line(OnelineCode(r'UPDATE_CELL_DISPLAY'))
+item2 = OnelineCode(r'setnumber pres_slot_num 0')
 
 init_block = LinesCode()
 init_block.add_line(OnelineCode('setnumber pres_subslot_num 0'))
@@ -64,18 +66,52 @@ condition = "pres_subslot_num > max_subslots"
 ifblock = IfBlockCode(condition)
 ifblock.if_true = OnelineCode('break')
 
-init_block1 = LinesCode()
-init_block1.add_line(OnelineCode('setnumber aaa 0'))
-init_block1.add_line(OnelineCode('calc aaa += 1'))
-condition = "aaa > 10"
+condition = "pres_subslot_num > max_subslots"
 ifblock1 = IfBlockCode(condition)
 ifblock1.if_true = OnelineCode('break')
+
+ddd = FuncBlock(FuncStructObj.name + "_loop1" + "_sub1")
+ddd1 = OnelineCode(r' DBG2 "Slot (%d/%d),  Run Status: [0x%x]\\n" pres_slot_num pres_subslot_num status')
+ddd2 = IfBlockCode("!(status & status_running) || (status & status_gold)")
+ddd22 = LinesCode()
+ddd22.add_line(OnelineCode('calc pres_subslot_num += 1'))
+ddd22.add_line(OnelineCode('continue'))
+ddd2.if_true = ddd22
+
+ddd3 = LoopCode()
+ddd3.ifblock = IfBlockCode("!(dbg & dbg_DoSpromChecking)")
+ddd3.ifblock.if_true = LinesCode()
+ddd3.ifblock.if_true.add_line(OnelineCode(r'type "Skipping SPROM Capture\\n"'))
+ddd3.ifblock.if_true.add_line(OnelineCode(r'break'))
+
+eee = FuncBlock(FuncStructObj.name + "_loop1" + "_sub1" + "_sub1")
+eee1 = OnelineCode(r' call Insieme_SpromCaptureSingleLine')
+eee2 = IfBlockCode("(status & status_running)")
+eee2.if_true = OnelineCode("call Insieme_SpromCheckToScanned")
+eee3 = IfBlockCode("(status & status_running)")
+eee3.if_true = OnelineCode("call Insieme_SpromCheckToCMPD")
+eee.items = [ddd1, ddd2, ddd3]
+ddd3.other_block = eee
+# ddd3 = IfBlockCode("!(dbg & dbg_DoSpromChecking)")
+# ddd3.if_true = OnelineCode(r'type "Skipping SPROM Capture\\n"')
+# ddd3_else = FuncBlock(FuncStructObj.name + "_loop1" + "_sub1" + "_elseCode")
+# ddd3_else_1 = OnelineCode(r'call Insieme_SpromCaptureSingleLine')
+# ddd3_else_2 = IfBlockCode("(status & status_running)")
+# ddd3_else_2.if_true = OnelineCode(r'call Insieme_SpromCheckToScanned')
+# ddd3_else_3 = IfBlockCode("(status & status_running)")
+# ddd3_else_3.if_true = OnelineCode(r'call Insieme_SpromCheckToCMPD')
+# ddd3_else.items = [ddd3_else_1, ddd3_else_2, ddd3_else_3]
+# ddd3.if_false = ddd3_else
+ddd4 = OnelineCode(r'calc pres_subslot_num += 1')
+ddd.items = [ddd1, ddd2, ddd3, ddd4, OnelineCode(r'continue')]
+
 temp = LoopCode()
-temp.init_block = init_block1
 temp.ifblock = ifblock1
-other_block = FuncBlock("haha")
-a = OnelineCode('hahaha')
-other_block.items = [temp, a]
+temp.other_block = ddd
+
+
+other_block = FuncBlock(FuncStructObj.name + "_loop1")  # 注意，一定要给名字(独一无二的名)
+other_block.items = [temp, OnelineCode(r'continue')]
 
 
 item3 = LoopCode()
@@ -84,7 +120,7 @@ item3.ifblock = ifblock
 item3.other_block = other_block
 # item3.other_block = temp
 # item3.other_block = OnelineCode('111')
-item4 = OnelineCode('EXIT')
+item4 = OnelineCode(r'EXIT "Insieme_SpromVerifyMain\\n"')
 FuncStructObj.items = [item1, item2, item3, item4]
 
 if __name__ == "__main__":
