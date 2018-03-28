e46e15951b844fe5dd5784c9a684c7c72137d229
TigerJlx <2274701670@qq.com>
2018-03-28 16:45:22 +0800

    代码优化

--- subgrap_try/subgrap1.py
+++ subgrap_try/subgrap1.py
@@ -4,31 +4,25 @@ grap_g = Digraph("G", format="pdf")
 
 sub_g0 = Digraph(name='cluster0', comment="process0", graph_attr={"style": 'filled', "color": 'lightgrey'},
                  node_attr={"style": "filled", "color": "white"})
-sub_g0.node("a0", "a0")
-sub_g0.node("a1", "a1")
-sub_g0.node("a2", "a2")
-sub_g0.node("a3", "a3")
-sub_g0.edges([("a0", "a1"), ("a1", "a2"), ("a2", "a3")])
+
+nodes = [("a0", "a0"), ("a1", "a1"), ("a2", "a2"), ("a3", "a3")]
+for node in nodes:
+    sub_g0.node(node[0], label=node[1])
+sub_g0.edges([("a0", "a1"), ("a1", "a2"), ("a2", "a3"), ("a3", "a0")])
 
 sub_g1 = Digraph(name='cluster1', comment="process1", graph_attr={"color": 'blue'},
                  node_attr={"style": "filled"})
-sub_g1.node("b0", "b0")
-sub_g1.node("b1", "b1")
-sub_g1.node("b2", "b2")
-sub_g1.node("b3", "b3")
+nodes = [("b0", "b0"), ("b1", "b1"), ("b2", "b2"), ("b3", "b3")]
+for node in nodes:
+    sub_g1.node(node[0], label=node[1])
 sub_g1.edges([("b0", "b1"), ("b1", "b2"), ("b2", "b3")])
-
-grap_g.node("start", label="start", shape="Mdiamond")
-grap_g.node("end", label="end", shape="Msquare")
 
 grap_g.subgraph(sub_g0)
 grap_g.subgraph(sub_g1)
-grap_g.edge("start", "a0")
-grap_g.edge("start", "b0")
 
-grap_g.edge("a1", "b3")
-grap_g.edge("b2", "a3")
+grap_g.node("start", label="start", shape="Mdiamond")
+grap_g.node("end", label="end", shape="Msquare")
+grap_g.edges([("start", "a0"), ("start", "b0"), ("a1", "b3"),
+              ("b2", "a3"), ("a3", "end"), ("b3", "end")])
 
-grap_g.edge("a3", "end")
-grap_g.edge("b3", "end")
 print(grap_g.source)
